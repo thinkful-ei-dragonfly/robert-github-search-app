a3c7bf93a130ff37637427d6385288217036d055
Robert Wiggins <wigginsro11@gmail.com>
2019-05-12 14:32:59 -0400

    added skeleton functions for api module

---
+++ scripts/api.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+const api = (function() {
+
+  
+  const BASE_GITHUB_API_URL = 'https://api.github.com';
+ 
+
+  /**
+   * 
+   * @param {*} params which are the arguments for the github fetch query 
+   */
+  function fetchGithubAPI (...params) {
+
+  }
+
+
+  /**
+   * 
+   * @param {*} username A string that represent the github username to search for and returns a 
+   * response in JSON form.
+   * 
+   * @returns A JSON reponse from the Github API
+   */
+  function getRepositories(username) {
+
+
+  }
+
+
+  
+
+
+
+
+  return {
+    getRepositories,
+  };
+
+
+
+})();
